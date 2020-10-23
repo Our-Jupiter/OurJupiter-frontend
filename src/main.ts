b3446906a25fb59b6f4c7b22e9dc2d4c97918dd9
HeeYeon <gmldus0407@naver.com>
2020-10-23 21:29:58 +0900

    Add: BaseAvatar component

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import 'vuesax/dist/vuesax.css';
 import 'material-icons/iconfont/material-icons.css';
 import BaseButton from '@/views/common/base-components/BaseButton.vue';
 import BaseCard from '@/views/common/base-components/BaseCard.vue';
+import BaseAvatar from '@/views/common/base-components/BaseAvatar.vue';
 
 Vue.config.productionTip = false;
 
@@ -14,6 +15,7 @@ Vue.use(Vuesax);
 
 Vue.component('BaseButton', BaseButton);
 Vue.component('BaseCard', BaseCard);
+Vue.component('BaseAvatar', BaseAvatar);
 
 new Vue({
   router,
---
+++ src/views/common/base-components/BaseAvatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <vs-avatar size="60">
+    <img v-if="img" class="avatar-img" :src="img" alt="" @click="clickEvent" />
+    <i v-else class="material-icons">person</i>
+  </vs-avatar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'BaseAvatar',
+  props: {
+    img: { type: String, required: false },
+  },
+  methods: {
+    clickEvent(event: Event) {
+      this.$emit('click', event);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.avatar-img {
+  cursor: pointer;
+}
+</style>
